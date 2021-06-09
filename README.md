b10753cb8ec72c05df908a3abb2fe74a04b2547f
xialvjun <xialvjun@didiglobal.com>
2021-06-09 15:08:46 +0800

    crawl with api

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # zhihu_favorites
- 爬取自己的知乎收藏夹，方便后续搜索（知乎烂透了）
+爬取自己的知乎收藏夹，方便后续搜索（知乎烂透了）。
+
+哪怕手机版有搜索，但搜索效果也很差。
+
+# usage
+```sh
+$ npm i -g @xialvjun/zhihu_favorites
+$ zhihu_favorites 'https://www.zhihu.com/people/xia-lu-jun-94/collections'
+```
--- src/abc.ts
+++ src/abc.ts
@@ -4,14 +4,23 @@ import better_sqlite3 from 'better-sqlite3';
 
 const db = better_sqlite3('db.sqlite3');
 
-db.exec(
-  `
-  create table colls (id primary key, name text, href text, desc text);
-  create table items (id text, coll_id references colls(id), url text, type text, title text, content text, author_name text, created_at);
-  create table kv (key primary key, value);
-  insert into kv values ('progress', '{}');
-  `,
+function check_db_table_exists(table_name: string) {
+  const table = check_db_table_exists.sql.get(table_name);
+  return table && table.name ? true : false;
+}
+check_db_table_exists.sql = db.prepare(
+  `SELECT name FROM sqlite_master WHERE type='table' AND name=?`,
 );
+if (!check_db_table_exists('kv')) {
+  db.exec(
+    `
+    create table colls (id primary key, name text, href text, desc text);
+    create table items (id text, coll_id references colls(id), url text, type text, title text, content text, author_name text, created_at);
+    create table kv (key primary key, value);
+    insert into kv values ('progress', '{}');
+    `,
+  );
+}
 
 const insert_coll = db.prepare(
   `insert into colls values (:id, :name, :href, :desc)`,
@@ -19,47 +28,58 @@ const insert_coll = db.prepare(
 const insert_item = db.prepare(
   `insert into items values (:id, :coll_id, :url, :type, :title, :content, :author_name, :created_at)`,
 );
+const get_progress = db.prepare(`select value from kv where key='progress'`);
+const set_progress = db.prepare(`update kv set value = ? where key='progress'`);
 
 function delay_random(min: number, max: number) {
   return new Promise((res) =>
     setTimeout(res, min + Math.random() * (max - min)),
   );
 };
-
-// type Coll = {
-//   id: string,
-//   name: string,
-//   href: string,
-//   desc: string;
-//   user_collections_url: string;
-// }
-// type Progress = {
-//   user_collections_url?: string;
-//   colls?: Coll[],
-//   coll_idx?: number;
-//   offset?: number;
-// }
+type Coll = {
+  id: string,
+  name: string,
+  href: string,
+  desc: string;
+  user_collections_url: string;
+}
+type Progress = {
+  user_collections_url?: string;
+  colls?: Coll[],
+  coll_idx?: number;
+  offset?: number;
+}
 
 async function main(user_collections_url: string) {
-  const collections_res = await axios.get(user_collections_url);
-  const colls_dom = cheerio.load(collections_res.data as string);
-  const colls_doms = Array.from(
-    colls_dom('#ProfileMain div.Card.SelfCollectionItem'),
-  );
-  const colls = colls_doms.map((dom) => {
-    const a = cheerio('a.SelfCollectionItem-title', dom);
-    const desc = cheerio('div.SelfCollectionItem-description', dom);
-    const href = a.attr('href')!;
-    const id = href.split('/').slice(-1)[0];
-    return { name: a.text(), href, id, desc: desc.text(), user_collections_url };
-  });
+  let progress: Progress = JSON.parse(get_progress.get().value);
+  if (!progress.colls || progress.user_collections_url !== user_collections_url) {
+    const collections_res = await axios.get(user_collections_url);
+    const colls_dom = cheerio.load(collections_res.data as string);
+    const colls_doms = Array.from(
+      colls_dom('#ProfileMain div.Card.SelfCollectionItem'),
+    );
+    const colls = colls_doms.map((dom) => {
+      const a = cheerio('a.SelfCollectionItem-title', dom);
+      const desc = cheerio('div.SelfCollectionItem-description', dom);
+      const href = a.attr('href')!;
+      const id = href.split('/').slice(-1)[0];
+      return { name: a.text(), href, id, desc: desc.text(), user_collections_url };
+    });
+    progress = { user_collections_url, colls };
+    set_progress.run(JSON.stringify(progress));
+  }
+  for (const coll of progress.colls!) {
+    const coll_idx = progress.colls!.indexOf(coll);
+    if (coll_idx < progress.coll_idx!) {
+      continue;
+    }
+    progress.coll_idx = coll_idx;
+    set_progress.run(JSON.stringify(progress));
 
-  for (const coll of colls!) {
     let offset = 0;
     insert_coll.run(coll);
     console.log(`inserted coll:`, coll.name);
     while (true) {
-      console.log(`axios.get:`, `https://www.zhihu.com/api/v4/collections/${coll.id}/items?offset=${offset}&limit=20`)
       const coll_items_res = await axios.get(
         `https://www.zhihu.com/api/v4/collections/${coll.id}/items`,
         { params: { offset, limit: 20 } },
@@ -107,10 +127,9 @@ async function main(user_collections_url: string) {
         console.log(`inserted item:`, title);
       }
       offset += 20;
-      // await delay_random(500, 2000);
+      await delay_random(500, 2000);
     }
   }
 }
 
-// main('https://www.zhihu.com/people/xia-lu-jun-94/collections');
-main(process.argv[2]);
+main('https://www.zhihu.com/people/xia-lu-jun-94/collections');
